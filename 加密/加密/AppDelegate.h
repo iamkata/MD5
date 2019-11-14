e44967c248d887e063a6141b228542e6188405c3
iamkata <35477358+iamkata@users.noreply.github.com>
2019-11-14 18:02:26 +0800

    Add files via upload

---
+++ 加密/加密/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  加密
+//
+//  Created by 徐金城 on 2019/11/14.
+//  Copyright © 2019 xujincheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 加密/加密/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  加密
+//
+//  Created by 徐金城 on 2019/11/14.
+//  Copyright © 2019 xujincheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
